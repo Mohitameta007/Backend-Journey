75e108417656e86a0cb056a09ab4eae8a05d207a
Mohit Ameta <ametamohit123@gmail.com>
2026-03-03 21:32:42 +0530

    updates code with http module

--- NODE.JS/basicNode.js
+++ NODE.JS/basicNode.js
@@ -8,4 +8,56 @@ fs.writeFile("newfile.txt" , "This is my new file" , function(err){
     else{
         console.log("done");
     }
+}) 
+
+fs.appendFile("newfile.txt" , "This is my appended code " , function(err){
+    if(err)
+    {
+        console.error(err)
+    }
+    else{
+        console.log("done");
+    }    
 })
+
+fs.rename("newfile.txt" , "renamefile.txt" , function(err){
+    if(err)
+    {
+        console.error(err)
+    }
+    else{
+        console.log("done");
+    }   
+})
+
+fs.copyFile("renamefile.txt" , "./copy.txt" , function(err){
+    if(err)
+    {
+        console.error(err.message)
+    }
+    else{
+        console.log("done");
+    }   
+})
+
+fs.unlink("copy.txt" , function(err){
+    if(err)
+    {
+        console.error(err.message)
+    }
+    else{
+        console.log("done");
+    }   
+})
+
+//-------------------------------------------------------- HTTP MODULE ----------------------------------------------------------------------------
+
+// basic server
+
+const http = require('http')
+
+const server = http.createServer((req , res)=>{
+    res.end("server chal");
+})
+
+server.listen(3000)
